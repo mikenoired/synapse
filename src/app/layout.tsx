18c0f1374d349a784966e75d134ff7fc42f032c3
Mike Noired <unkstd@mail.ru>
2025-09-23 17:38:47 +0300

    feat: add WebVitals component for performance monitoring in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 import "./globals.css";
+import { WebVitals } from "@/shared/lib/webvitals";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="ru" suppressHydrationWarning>
       <body className={inter.className}>
+        <WebVitals />
         <Providers>
           {children}
         </Providers>
