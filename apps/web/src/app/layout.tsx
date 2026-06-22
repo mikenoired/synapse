4ed9dfb8e3d1a6da7769876fbaa401368f96f43e
Mike Noired <unkstd@mail.ru>
2026-06-22 21:31:17 +0300

    replate global default font to geist

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist_Mono } from "next/font/google";
-import localFont from "next/font/local";
+import { Geist } from "next/font/google";
 import type { ReactNode } from "react";
 
 import { Providers } from "@/app/providers";
@@ -10,20 +10,12 @@ import { WebVitals } from "@/shared/lib/webvitals";
 
 import "./globals.css";
 
-const nonBureau = localFont({
-	src: [
-		{ path: "./fonts/NonBureau-Regular.woff2", weight: "400" },
-		{ path: "./fonts/NonBureau-Medium.woff2", weight: "500" },
-		{ path: "./fonts/NonBureau-SemiBold.woff2", weight: "600" },
-		{ path: "./fonts/NonBureau-Bold.woff2", weight: "700" },
-	],
-	variable: "--font-non-bureau",
-});
-
 const geistMono = Geist_Mono({
 	subsets: ["latin"],
 	variable: "--font-geist-mono",
 });
+
+const geist = Geist()
 
 export const metadata: Metadata = {
 	title: "Synapse — личный архив",
@@ -49,7 +41,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 	const { user } = await createContext({});
 
 	return (
-		<html lang="ru" className={`${nonBureau.variable} ${geistMono.variable}`} suppressHydrationWarning>
+		<html lang="ru" className={`${geist} ${geistMono.variable}`} suppressHydrationWarning>
 			<head>
 				<link rel="preconnect" href="http://localhost:9000" />
 				<link rel="dns-prefetch" href="http://localhost:9000" />
